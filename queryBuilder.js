21f73729a69f7edd15db6b9dc4ef6afbbcbe9be2
Chris Pedersen <chrisaped@gmail.com>
2019-05-02 14:41:20 +0200

    demo data, third party plugins

--- queryBuilder.js
+++ queryBuilder.js
@@ -1,86 +1,186 @@
-$( document ).ready(function() {
-  var rules_basic = {
-    condition: 'AND',
+$(document).ready(function() {
+  var rules_widgets = {
+    condition: 'OR',
     rules: [{
-      id: 'price',
-      operator: 'less',
-      value: 10.25
+      id: 'date',
+      operator: 'equal',
+      value: '1991/11/17'
     }, {
-      condition: 'OR',
+      id: 'rate',
+      operator: 'equal',
+      value: 22
+    }, {
+      id: 'category',
+      operator: 'equal',
+      value: '38'
+    }, {
+      condition: 'AND',
       rules: [{
-        id: 'category',
+        id: 'coord',
         operator: 'equal',
-        value: 2
-      }, {
-        id: 'category',
-        operator: 'equal',
-        value: 1
+        value: 'B.3'
       }]
     }]
   };
 
-  $('#builder-basic').queryBuilder({
+  // Fix for Selectize
+  $('#builder-widgets').on('afterCreateRuleInput.queryBuilder', function(e, rule) {
+    if (rule.filter.plugin == 'selectize') {
+      rule.$el.find('.rule-value-container').css('min-width', '200px')
+        .find('.selectize-control').removeClass('form-control');
+    }
+  });
+
+  // Fix for Bootstrap Datepicker
+  $('#builder-widgets').on('afterUpdateRuleValue.queryBuilder', function(e, rule) {
+    if (rule.filter.plugin === 'datepicker') {
+      rule.$el.find('.rule-value-container input').datepicker('update');
+    }
+  });
+
+  $('#builder-widgets').queryBuilder({
     filters: [{
-      id: 'name',
-      label: 'Name',
-      type: 'string'
-    }, {
-      id: 'category',
-      label: 'Category',
-      type: 'integer',
-      input: 'select',
-      values: {
-        1: 'Books',
-        2: 'Movies',
-        3: 'Music',
-        4: 'Tools',
-        5: 'Goodies',
-        6: 'Clothes'
-      },
-      operators: ['equal', 'not_equal', 'in', 'not_in', 'is_null', 'is_not_null']
-    }, {
-      id: 'in_stock',
-      label: 'In stock',
-      type: 'integer',
-      input: 'radio',
-      values: {
-        1: 'Yes',
-        0: 'No'
-      },
-      operators: ['equal']
-    }, {
-      id: 'price',
-      label: 'Price',
-      type: 'double',
+      id: 'date',
+      label: 'Datepicker',
+      type: 'date',
       validation: {
-        min: 0,
-        step: 0.01
+        format: 'YYYY/MM/DD'
+      },
+      plugin: 'datepicker',
+      plugin_config: {
+        format: 'yyyy/mm/dd',
+        todayBtn: 'linked',
+        todayHighlight: true,
+        autoclose: true
       }
     }, {
-      id: 'id',
-      label: 'Identifier',
-      type: 'string',
-      placeholder: '____-____-____',
-      operators: ['equal', 'not_equal'],
+      id: 'rate',
+      label: 'Slider',
+      type: 'integer',
       validation: {
-        format: /^.{4}-.{4}-.{4}$/
+        min: 0,
+        max: 100
+      },
+      plugin: 'slider',
+      plugin_config: {
+        min: 0,
+        max: 100,
+        value: 0
+      },
+      valueSetter: function(rule, value) {
+        if (rule.operator.nb_inputs == 1) value = [value];
+        rule.$el.find('.rule-value-container input').each(function(i) {
+          $(this).slider('setValue', value[i] || 0);
+        });
+      },
+      valueGetter: function(rule) {
+        var value = [];
+        rule.$el.find('.rule-value-container input').each(function() {
+          value.push($(this).slider('getValue'));
+        });
+        return rule.operator.nb_inputs == 1 ? value[0] : value;
+      }
+    }, {
+      id: 'category',
+      label: 'Selectize',
+      type: 'string',
+      plugin: 'selectize',
+      plugin_config: {
+        valueField: 'id',
+        labelField: 'name',
+        searchField: 'name',
+        sortField: 'name',
+        create: true,
+        maxItems: 1,
+        plugins: ['remove_button'],
+        onInitialize: function() {
+          var that = this;
+
+          if (localStorage.demoData === undefined) {
+            $.getJSON('demo_data.json', function(data) {
+              localStorage.demoData = JSON.stringify(data);
+              data.forEach(function(item) {
+                that.addOption(item);
+              });
+            });
+          }
+          else {
+            JSON.parse(localStorage.demoData).forEach(function(item) {
+              that.addOption(item);
+            });
+          }
+        }
+      },
+      valueSetter: function(rule, value) {
+        rule.$el.find('.rule-value-container input')[0].selectize.setValue(value);
+      }
+    }, {
+      id: 'coord',
+      label: 'Coordinates',
+      type: 'string',
+      validation: {
+        format: /^[A-C]{1}.[1-6]{1}$/
+      },
+      input: function(rule, name) {
+        var $container = rule.$el.find('.rule-value-container');
+
+        $container.on('change', '[name='+ name +'_1]', function(){
+          var h = '';
+
+          switch ($(this).val()) {
+            case 'A':
+              h = '<option value="-1">-</option> <option value="1">1</option> <option value="2">2</option>';
+              break;
+            case 'B':
+              h = '<option value="-1">-</option> <option value="3">3</option> <option value="4">4</option>';
+              break;
+            case 'C':
+              h = '<option value="-1">-</option> <option value="5">5</option> <option value="6">6</option>';
+              break;
+          }
+
+          $container.find('[name$=_2]')
+            .html(h).toggle(!!h)
+            .val('-1').trigger('change');
+        });
+
+        return '\
+        <select name="'+ name +'_1"> \
+          <option value="-1">-</option> \
+          <option value="A">A</option> \
+          <option value="B">B</option> \
+          <option value="C">C</option> \
+        </select> \
+        <select name="'+ name +'_2" style="display:none;"></select>';
+      },
+      valueGetter: function(rule) {
+        return rule.$el.find('.rule-value-container [name$=_1]').val()
+          +'.'+ rule.$el.find('.rule-value-container [name$=_2]').val();
+      },
+      valueSetter: function(rule, value) {
+        if (rule.operator.nb_inputs > 0) {
+          var val = value.split('.');
+
+          rule.$el.find('.rule-value-container [name$=_1]').val(val[0]).trigger('change');
+          rule.$el.find('.rule-value-container [name$=_2]').val(val[1]).trigger('change');
+        }
       }
     }],
 
-    rules: rules_basic
+    rules: rules_widgets
   });
 
   $('#btn-reset').on('click', function() {
-    $('#builder-basic').queryBuilder('reset');
+    $('#builder-widgets').queryBuilder('reset');
   });
 
   $('#btn-set').on('click', function() {
-    $('#builder-basic').queryBuilder('setRules', rules_basic);
+    $('#builder-widgets').queryBuilder('setRules', rules_widgets);
   });
 
   $('#btn-get').on('click', function() {
-    var result = $('#builder-basic').queryBuilder('getRules');
-    
+    var result = $('#builder-widgets').queryBuilder('getRules');
+
     if (!$.isEmptyObject(result)) {
       alert(JSON.stringify(result, null, 2));
     }
