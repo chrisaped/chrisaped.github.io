09372fc664b6fe7eb68de377d8e9bbf753bf7ae1
Chris Pedersen <chrisaped@gmail.com>
2019-05-03 10:33:37 +0200

    more changes

--- queryBuilder.js
+++ queryBuilder.js
@@ -1,43 +1,9 @@
 $(document).ready(function() {
-  var rules_widgets = {
-    condition: 'OR',
-    rules: [{
-      id: 'event',
-      operator: 'equal',
-      value: 'event'
-    }]
-  };
-
-  // Fix for Selectize
-  $('#builder-widgets').on('afterCreateRuleInput.queryBuilder', function(e, rule) {
-    if (rule.filter.plugin == 'selectize') {
-      rule.$el.find('.rule-value-container').css('min-width', '200px')
-        .find('.selectize-control').removeClass('form-control');
-    }
-  });
-
-  // Fix for Bootstrap Datepicker
-  $('#builder-widgets').on('afterUpdateRuleValue.queryBuilder', function(e, rule) {
-    if (rule.filter.plugin === 'datepicker') {
-      rule.$el.find('.rule-value-container input').datepicker('update');
-    }
-  });
-
-  $('#builder-widgets').queryBuilder({
+  $('#builder-basic').queryBuilder({
     filters: [{
-      id: 'date',
-      label: 'Date',
-      type: 'date',
-      validation: {
-        format: 'YYYY/MM/DD'
-      },
-      plugin: 'datepicker',
-      plugin_config: {
-        format: 'yyyy/mm/dd',
-        todayBtn: 'linked',
-        todayHighlight: true,
-        autoclose: true
-      }
+      id: 'something',
+      label: 'Something',
+      type: 'string'
     },
     {
       id: 'event',
@@ -45,7 +11,7 @@ $(document).ready(function() {
       type: 'string',
       plugin: 'selectize',
       plugin_config: {
-        valueField: 'id',
+        valueField: 'name',
         labelField: 'name',
         searchField: 'name',
         sortField: 'name',
@@ -58,6 +24,7 @@ $(document).ready(function() {
           if (localStorage.demoData === undefined) {
             $.getJSON('demo_data.json', function(data) {
               localStorage.demoData = JSON.stringify(data);
+              console.log('data', data);
               data.forEach(function(item) {
                 that.addOption(item);
               });
@@ -73,24 +40,11 @@ $(document).ready(function() {
       valueSetter: function(rule, value) {
         rule.$el.find('.rule-value-container input')[0].selectize.setValue(value);
       }
-    }],
-
-    rules: rules_widgets
+    }]
   });
 
   $('#btn-reset').on('click', function() {
-    $('#builder-widgets').queryBuilder('reset');
+    $('#builder-basic').queryBuilder('reset');
   });
 
-  $('#btn-set').on('click', function() {
-    $('#builder-widgets').queryBuilder('setRules', rules_widgets);
-  });
-
-  $('#btn-get').on('click', function() {
-    var result = $('#builder-widgets').queryBuilder('getRules');
-
-    if (!$.isEmptyObject(result)) {
-      alert(JSON.stringify(result, null, 2));
-    }
-  });
 });
